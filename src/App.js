b2b2dbfa5e6ee9ae440c7038247a23b76e812797
Ben Aidlin <ben.aidlin@intel.com>
2022-10-04 12:21:26 +0300

    have navnar, have students, no navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Body from './components/Body';
+import './components/Header';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    let coach_name = "Pini Hadida";
+    return (
+      <div className="App">
+        <Navbar></Navbar>
+        <Header title={coach_name}/>        
+        <Body coach_name={coach_name}/>
+      </div>
+    );
+  }    
 }
 
 export default App;
