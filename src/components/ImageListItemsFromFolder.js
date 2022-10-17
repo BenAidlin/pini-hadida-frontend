f95b997edd89824b8222ecb3f6e596fcbc29883f
Ben Aidlin <ben.aidlin@intel.com>
2022-10-17 14:13:11 +0300

    create imageListItemsFromFolder and use in Gallery

---
+++ src/components/ImageListItemsFromFolder.js
@@ -0,0 +1,26 @@
+import { ImageListItem } from "@mui/material";
+
+function importAll(r) {
+    let images = {};
+    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}  
+const ImageListItemsFromFolder = (props) => {    
+    const handleImageClick = props.handleImageClick;   
+    const context = props.context;        
+    const images = importAll(context);
+    return (
+        Object.values(images).map((item) => (
+            <ImageListItem key={item}>
+            <img 
+                src={`${item}`}
+                srcSet={`${item}`}
+                alt={item}
+                loading="lazy"
+                onClick={()=>{handleImageClick(item)}}
+            />
+            </ImageListItem>
+        ))
+    )
+}
+export default ImageListItemsFromFolder;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,16 +1,11 @@
 
 import { useState } from "react";
 import { ImageList, ImageListItem, Modal } from "@mui/material";
-import '../style/Gallery.css'
-function importAll(r) {
-    let images = {};
-    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-  
+import '../style/Gallery.css';
+import ImageListItemsFromFolder from './../components/ImageListItemsFromFolder';
+
 const Gallery = (props) => {
-    const theme = props.theme;
-    const images = importAll(require.context('../extensions/images/gallery'))
+    const theme = props.theme;    
     const [open, setOpen] = useState(false);
     const [imgInModal, setImageInModal] = useState('');
     const handleOpen = (src) => {setOpen(true); setImageInModal(src);}
@@ -28,17 +23,10 @@ const Gallery = (props) => {
             </Modal>  
 
             <ImageList className="imglist" sx={{margin: 'auto', paddingBottom:20, }} gap={10}>
-            {Object.values(images).map((item) => (
-                <ImageListItem key={item}>
-                <img 
-                    src={`${item}`}
-                    srcSet={`${item}`}
-                    alt={item}
-                    loading="lazy"
-                    onClick={()=>{handleOpen(item)}}
-                />
-                </ImageListItem>
-            ))}
+                <ImageListItemsFromFolder 
+                context={require.context('../extensions/images/gallery' )}
+                handleImageClick={(item) => handleOpen(item)}
+                />                
             </ImageList>
         </div>        
     );
