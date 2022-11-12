6c0acf87bb3804dcc514f6b9597a14bb3d1f0489
Ben Aidlin <ben.aidlin@intel.com>
2022-11-12 13:00:16 +0200

    use grid to display students

--- src/components/AdminStudentManager.js
+++ src/components/AdminStudentManager.js
@@ -4,60 +4,58 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import ManageAccountsSharpIcon from '@mui/icons-material/ManageAccountsSharp';
 import PersonAddSharpIcon from '@mui/icons-material/PersonAddSharp';
-import { Paper, Typography, Pagination } from "@mui/material";
+import { Paper, Typography, } from "@mui/material";
+import { Box } from "@mui/system";
 
 const AdminStudentManager = (props) => {
     const users = props.users;
     const potentials = props.potentials;
     const theme = props.theme;
-    const [onSigned, setOnSigned] = useState(true);
-
+    const [onSigned, setOnSigned] = useState(true);    
     const listOfUsers = []; 
+    
     users.forEach((user, index) => {
-        if(index % 2 === 0){
+        if(index % 3 === 0){
             listOfUsers.push([]);
         }
         listOfUsers[listOfUsers.length - 1].push(user);        
     });
-    const [userPage, setUserPage] = useState(1);
 
     const listOfPoten = []; 
     potentials.forEach((poten, index) => {
-        if(index % 2 === 0){
+        if(index % 3 === 0){
             listOfPoten.push([]);
         }
         listOfPoten[listOfPoten.length - 1].push(poten);        
     });
-    const [potenPage, setPotenPage] = useState(1);
 
     return (
         <div style={{ backgroundColor: theme.palette.decorative.darkGrey, textAlign: 'center', minHeight: '100vh'}}>            
         <Typography variant="h5" fontFamily={theme.typography.fontFamily}
             color={theme.palette.decorative.lightBrown} dir={'rtl'}>
             <div style={{display: onSigned ? 'block' : 'none'}}>
-                <div style={{ display:'block',  marginTop: '5vh'}}>
+                <div style={{ display:'block',  marginTop: '5vh', paddingBottom: '3vh'}}>
                     תלמידים פעילים  
+                    <p style={{fontSize: 'medium'}}>לעריכת פרטי תלמיד לחץ על התלמיד</p>
                 </div>
-                <div style={{display: onSigned ? 'flex' : 'none' , position: 'relative', paddingBottom: '10vh'}}>                
-                    {listOfUsers[userPage - 1].map(u => <UserData theme={theme} userData={u}></UserData>)}                    
-                </div>
-                <div dir="ltr" style={{margin: 'auto'}}>
-                        <Pagination color="secondary" size="large" sx={{ position: 'absolute', right: '50vw' }}
-                        count={listOfUsers.length} page={userPage} onChange={(event, value)=>setUserPage(value)} />
-                </div>                
+
+                <Box sx={{ display: 'grid', gridTemplateColumns: {
+                    xs: 'repeat(2, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)', xl: 'repeat(4, 1fr)'}, 
+                    paddingBottom: '10vh'}} justifyContent='center' alignItems="center">
+                    {users.map(u =><Box justifyContent='center'><UserData theme={theme} userData={u}></UserData></Box>)}
+                </Box>                
             </div>    
 
-            <div style={{display: !onSigned ? 'block' : 'none'}}>
+            <div style={{display: !onSigned ? 'block' : 'none', justifyContent: 'center'}}>
                 <div style={{ display:'block',  marginTop: '5vh'}}>
                     משתמשים שהתחברו 
+                    <p style={{fontSize: 'medium'}}>להוספת משתמש לתלמידים לחץ על המשתמש</p>
                 </div>
-                <div style={{width: '80vw', display: !onSigned ? 'flex' : 'none' , paddingBottom: '10vh'}}>                
-                    {listOfPoten[potenPage - 1].map(u => <UserData theme={theme} userData={u}></UserData>)}
-                </div>
-                <div dir="ltr" style={{margin: 'auto'}}>
-                        <Pagination color="secondary" size="large" sx={{ position: 'absolute', right: '50vw' }}
-                        count={listOfPoten.length} page={potenPage} onChange={(event, value)=>setPotenPage(value)} />
-                </div>                
+                <Box sx={{ display: 'grid', gridTemplateColumns: {
+                    xs: 'repeat(2, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)', xl: 'repeat(4, 1fr)'}, 
+                    paddingBottom: '10vh', }} justifyContent='center'  alignItems="center">
+                    {potentials.map(p => <Box justifyContent='center'><UserData theme={theme} userData={p}></UserData></Box>)}
+                </Box>                
             </div>  
 
             <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
