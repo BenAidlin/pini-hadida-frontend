31d98ee60d550388261fa32c2e55a44cb64a4554
Ben Aidlin <ben.aidlin@intel.com>
2022-11-12 11:43:39 +0200

    change pagintation location

--- src/components/AdminStudentManager.js
+++ src/components/AdminStudentManager.js
@@ -38,24 +38,28 @@ const AdminStudentManager = (props) => {
                 <div style={{ display:'block',  marginTop: '5vh'}}>
                     תלמידים פעילים  
                 </div>
-                <div style={{width: '80vw', display: onSigned ? 'flex' : 'none' , paddingBottom: '10vh'}}>                
+                <div style={{display: onSigned ? 'flex' : 'none' , position: 'relative', paddingBottom: '10vh'}}>                
                     {listOfUsers[userPage - 1].map(u => <UserData theme={theme} userData={u}></UserData>)}                    
                 </div>
                 <div dir="ltr" style={{margin: 'auto'}}>
-                        <Pagination color="secondary" size="large"
+                        <Pagination color="secondary" size="large" sx={{ position: 'absolute', right: '50vw' }}
                         count={listOfUsers.length} page={userPage} onChange={(event, value)=>setUserPage(value)} />
                 </div>                
-            </div>                
+            </div>    
 
-            
-            <div style={{width: '30vw',display: !onSigned ? 'inline-block' : 'none' }}>
-                משתמשים שהתחברו
-                {listOfPoten[potenPage - 1].map(u => <UserData theme={theme} userData={u}></UserData>)}
-                <div dir="ltr">
-                    <Pagination color="secondary" size="large"
-                    count={listOfPoten.length} page={potenPage} onChange={(event, value)=>setPotenPage(value)} />
-                </div>        
-            </div>
+            <div style={{display: !onSigned ? 'block' : 'none'}}>
+                <div style={{ display:'block',  marginTop: '5vh'}}>
+                    משתמשים שהתחברו 
+                </div>
+                <div style={{width: '80vw', display: !onSigned ? 'flex' : 'none' , paddingBottom: '10vh'}}>                
+                    {listOfPoten[potenPage - 1].map(u => <UserData theme={theme} userData={u}></UserData>)}
+                </div>
+                <div dir="ltr" style={{margin: 'auto'}}>
+                        <Pagination color="secondary" size="large" sx={{ position: 'absolute', right: '50vw' }}
+                        count={listOfPoten.length} page={potenPage} onChange={(event, value)=>setPotenPage(value)} />
+                </div>                
+            </div>  
+
             <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
             <BottomNavigation
                 showLabels
