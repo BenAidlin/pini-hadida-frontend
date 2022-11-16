8f9422ea38ebc0e71aaea62b143b97d3ade1dfcd
Ben Aidlin <ben.aidlin@intel.com>
2022-11-16 17:48:51 +0200

    adjust add student dialog

--- src/components/AddStudentDialog.js
+++ src/components/AddStudentDialog.js
@@ -9,38 +9,60 @@ import DialogTitle from '@mui/material/DialogTitle';
 import UserData from "../components/UserData";
 import { MenuItem, Select, InputLabel } from '@mui/material';
 import { useState } from 'react';
+import {format} from 'date-fns';
+import { useEffect } from 'react';
+
 export default function AddStudentDialog(props) {
   const [open, setOpen] = React.useState(true);
   const theme = props.theme;
   const potentialData = props.potentialData;
   const onClose = props.onClose;
-  const [rank, setrank] = useState('white');
-  const [subDate, setSubDate] = useState('16/11/2022');
-  const [joinDate, setJoinDate] = useState('16/11/2022');
+  const [ddValue, setddValue] = useState('white');
+  const [subDate, setSubDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+  const [subTime, setSubTime] = useState(3);
+  const [joinDate, setJoinDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+  const [addDisabled, setAddDisabled] = useState(true);
+  const belts = ['white', 'blue', 'purple', 'brown', 'black']
   const handleClose = () => {
     onClose();
     setOpen(false);
   };
-  const handleDDChange = (value) => {
-
+  const handleDDChange = (value) => {    
+    setddValue(value.target.value);
   }
-  const addStudent = () => {
-    // send request..
+  const addStudent = async () => {
+    // send request..  
+    setAddDisabled(false);
+    const addReq = await fetch(process.env.REACT_APP_api_route + '/users/user/' 
+        + potentialData._id + '?rank='+ddValue 
+        + '&lastSubscriptionDate='+subDate
+        + '&joinDate='+joinDate
+        + '&subscriptionTime='+subTime.toString(), {
+        method: 'POST',
+        headers:{
+          'Content-Type' : 'application/json'
+        }
+    });
+    console.log(addReq);
     setOpen(false);
   }
+  useEffect(()=>{
+    if(addDisabled && joinDate && subTime && subDate && ddValue) setAddDisabled(false);
+  }, [addDisabled,joinDate,subTime,subDate,ddValue])
   return (
     <div >
       <Dialog open={open} onClose={handleClose} dir='rtl'>
-        <DialogTitle dir={'rtl'}>הוספת תלמיד</DialogTitle>
+        <DialogTitle dir={'rtl'}>הוספת תלמיד - {potentialData.name}</DialogTitle>
         <DialogContent>
           <DialogContentText dir='rtl'>
-            להוספת תלמיד יש להוסיף תאריך כניסה למועדון ותאריך תחילת מנוי.
+            להוספת תלמיד יש להוסיף תאריך כניסה למועדון, תאריך תחילת מנוי ודרגת התלמיד.
           </DialogContentText>
-          <div style={{backgroundColor:theme.palette.decorative.darkGrey}}>
+          <div >
                 <UserData userData={potentialData} theme={theme}></UserData>
             </div>
           <TextField
             autoFocus
+            value={joinDate}
             margin="dense"
             id="joinDate"
             label="תאריך כניסה למועדון"
@@ -48,36 +70,53 @@ export default function AddStudentDialog(props) {
             variant="standard"
             InputLabelProps={{ shrink: true }}
             dir={'rtl'}
+            onChange={(value)=>{setJoinDate(value.target.value);}}
           />
           <TextField
             autoFocus
+            value={subDate}
             margin="dense"
             id="subscription"
             label="תאריך מנוי אחרון"
             type="date"            
             variant="standard"
             InputLabelProps={{ shrink: true }}
+            dir={'rtl'}
+            onChange={(value)=>{setSubDate(value.target.value);}}
+          />
+        <TextField
+            autoFocus
+            value={subTime}
+            margin="dense"
+            id="subscription"
+            label="זמן תשלום מנוי בחודשים"
+            type="number"            
+            variant="standard"
+            InputLabelProps={{ shrink: true }}
+            dir={'ltr'}
+            onChange={(value)=>{setSubTime(value.target.value);}}
           />
         <InputLabel id="select-rank" dir='rtl'>חגורה</InputLabel>
         <Select
             labelId="select-rank"
             id="demo-simple-select"
-            value={''}
+            value={ddValue}
             label="rank"
             onChange={handleDDChange}
-            dir={'rtl'}
-            
+            dir={'rtl'}            
         >
-            <MenuItem value={'white'}>לבן</MenuItem>
-            <MenuItem value={'blue'}>כחול</MenuItem>
-            <MenuItem value={'purple'}>סגול</MenuItem>
-            <MenuItem value={'brown'}>חום</MenuItem>
-            <MenuItem value={'black'}>שחור</MenuItem>
+            {
+                belts.map(b => (
+                    <MenuItem dir='rtl' value={b}><img style={{maxWidth:'15%'}} src={require('./../extensions/images/profile/'+ b+'.png')} alt={"no rank"}></img></MenuItem>
+                ))
+            }            
         </Select>
+        
+
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>ביטול</Button>
-          <Button onClick={addStudent}>הוסף תלמיד</Button>
+          <Button disabled={addDisabled} onClick={addStudent}>הוסף תלמיד</Button>
         </DialogActions>
       </Dialog>
     </div>
